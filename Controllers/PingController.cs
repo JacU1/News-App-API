0c72987ba3ca27d55840cb534e87eee9efd6b9aa
Jacek <jacunio122@gmail.com>
2023-09-01 10:27:07 +0200

    Security #1

---
+++ Controllers/PingController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace News_App_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+
+        private readonly IAntiforgery? _antiforgery;
+
+        public PingController(IAntiforgery? antiforgery)
+        {
+            _antiforgery = antiforgery;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var aft = _antiforgery!.GetAndStoreTokens(HttpContext);
+            Response.Cookies.Append("XSRF-TOKEN", aft.RequestToken!, new CookieOptions
+            {
+                HttpOnly = false,
+                Secure = false,
+            });
+
+            return Ok(true);
+        }
+    }
+}
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using News_App_API.Interfaces;
-using System.Configuration;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace News_App_API.Services
 {
